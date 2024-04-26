fd2a9b801abdc8c4acb6d4d03e1aa78b818e02ca
youssef <yousseftaoussi894@gmail.com>
2024-04-26 13:28:45 +0100

    .

--- XO.js
+++ XO.js
@@ -1,16 +1,13 @@
 let playerText = document.getElementById("player-text");
 let restartButton = document.getElementById("reset-btn");
 let boxes = Array.from(document.getElementsByClassName("btn"));
-let container = document.getElementsByClassName("container")
+let container = document.getElementsByClassName("container");
 let winnerdisplay = document.getElementById("winnerdisplay");
 let nameOfTheWinner = document.querySelector(".winner  span");
 let resetbtn = document.getElementById("resetbtn");
-let spaces = [];
-spaces.length = 9;
-const O_TEXT = "O";
-const X_TEXT = "X";
-let currentPalyer = X_TEXT;
-let winning = [
+class xAndO {
+  marks = [];
+  winCases = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -19,35 +16,43 @@ let winning = [
     [1, 4, 7],
     [2, 5, 8],
     [2, 4, 6],
-];
+  ];
+  currentPlayer = "X";
 
-function reloadpage() {
-    resetbtn.addEventListener("click", () => {
-        location.reload();
-    })
-};
-reloadpage();
-function chekTheWinner() {
-    let cheking = "";
-    for (let e of winning) {
-        if (spaces[e[0]] == spaces[e[1]] && spaces[e[0]] == spaces[e[2]] && spaces[e[0]] !== undefined) {
-            winnerdisplay.style.display = "flex";
-            container[0].classList.add("eventnone")
-            nameOfTheWinner.innerText += spaces[e[0]]
-        }
-    }
-};
-
-function marqueXAndO() {
-    boxes.forEach((e) => {
-        e.addEventListener(("click"), () => {
-            e.innerText = currentPalyer;
-            currentPalyer = currentPalyer == X_TEXT ? O_TEXT : X_TEXT;
-            spaces[+e.id] = e.innerText;
-            console.log(spaces[e.id]);
-            console.log(spaces);
-            chekTheWinner();
-        })
+  constructor() {
+    console.log("running");
+    this.marks.length = 9;
+    boxes.forEach((box) => {
+      box.addEventListener("click", () => {
+        const boxId = +box.id;
+        this.currentPalyer = this.currentPalyer === "X" ? "O" : "X";
+        box.innerText = this.currentPalyer;
+        this.marks[boxId] = this.currentPalyer;
+        this.validateBoard();
+      });
     });
-};
-marqueXAndO();
+    resetbtn.addEventListener("click", () => {
+      this.currentPalyer = "X";
+      this.marks = this.marks.map((mark) => undefined);
+      winnerdisplay.style.display = "none";
+      container[0].classList.remove("eventnone");
+    });
+  }
+
+  validateBoard() {
+    for (let i = 0; i < this.winCases.length; i++) {
+      let currentWinCase = this.winCases[i];
+      if (
+        this.marks[currentWinCase[0]] !== undefined &&
+        this.marks[currentWinCase[0]] === this.marks[currentWinCase[1]] &&
+        this.marks[currentWinCase[0]] === this.marks[currentWinCase[2]]
+      ) {
+        winnerdisplay.style.display = "flex";
+        nameOfTheWinner.innerText === this.marks[currentWinCase[0]];
+        container[0].classList.add("eventnone");
+        break;
+      }
+    }
+  }
+}
+const startGame = new xAndO();
